b15b4ee0b3a383d3cfe02f8c9ed05f8349b573a3
Frank Li <frankla0203@outlook.com>
2023-12-16 17:30:51 -0800

    Add tabler icons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        text: "#ecedf4",
+        background: "#131420",
+        primary: "#836ba8",
+        secondary: "#150d16",
+        accent: "#a16ba8",
+      },
+    },
+  },
+  plugins: [],
+};
