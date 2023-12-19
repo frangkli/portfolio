631263e33be1db880da2deb9fc630458e3a6b7cd
Frank Li <frankla0203@outlook.com>
2023-12-19 00:54:42 -0800

    Better link component and socials

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
   export let href: string;
-  export let text: string = "";
+  export let noLine: boolean = false;
 </script>
 
 <a
   {href}
   on:mousemove={(event) => {
+    const text = event.currentTarget;
     const modal = event.currentTarget.children[0];
-    if (modal instanceof HTMLElement) {
-      modal.style.top = `${event.clientY + 10}px`;
-      if (text !== "") {
-        modal.style.left = `${event.offsetX + 10}px`;
-      } else {
-        modal.style.left = `${event.clientX + 10}px`;
+    if (modal instanceof HTMLElement && text instanceof HTMLElement) {
+      const selfLeft = text.getBoundingClientRect().left;
+      const parentLeft = text.parentElement?.getBoundingClientRect().left;
+      let diff = 0;
+      if (parentLeft && selfLeft - parentLeft > event.offsetX) {
+        diff = selfLeft - parentLeft;
       }
+      modal.style.left = `${event.offsetX + diff + 10}px`;
+      modal.style.top = `${event.clientY + 10}px`;
     }
   }}
   on:mouseenter={(event) => {
@@ -28,11 +31,7 @@
   <div class="absolute bg-black text-white hidden font-mono px-2 py-1 text-xs">
     {href}
   </div>
-  {#if text !== ""}
-    {text}
-  {:else}
-    <span class="underline underline-offset-2">
-      <slot />
-    </span>
-  {/if}
+  <span class={noLine ? "" : "underline underline-offset-2"}>
+    <slot />
+  </span>
 </a>
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Link from "./Link.svelte";
+  import {
+    IconBrandGithub,
+    IconBrandLinkedin,
+    IconMail,
+  } from "@tabler/icons-svelte";
+</script>
+
+<aside class="h-screen py-64 sticky top-0">
+  <div class="flex flex-col gap-10 h-full">
+    <div class="w-max">
+      <h1 class="italic text-5xl font-cursive">Frank Li</h1>
+    </div>
+    <div class="container grow text-right">
+      <ul class="leading-loose">
+        <li><Link href="/" noLine>Home</Link></li>
+        <li><Link href="/cv" noLine>CV</Link></li>
+        <li><Link href="/projects" noLine>Projects</Link></li>
+        <li><Link href="/academics" noLine>Academics</Link></li>
+        <li><Link href="/tech" noLine>Tech</Link></li>
+      </ul>
+    </div>
+    <div class="container items-center flex flex-row gap-5">
+      <Link href="https://github.com/frangkli"><IconBrandGithub /></Link>
+
+      <Link href="https://linkedin.com/in/anglifrank"
+        ><IconBrandLinkedin /></Link
+      >
+      <Link href="mailto://frankla0203@outlook.com"><IconMail /></Link>
+    </div>
+    <div>
+      <p>&copy; 2023 Frank Li.</p>
+    </div>
+  </div>
+</aside>
