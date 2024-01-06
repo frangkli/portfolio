f312681b1880cea86e0ba4bbfa21db87a8845335
Frank Li <frankla0203@outlook.com>
2024-01-06 15:11:44 -0800

    Unresponsive CV draft

---
+++ src/components/Experience.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  type Experience = {
+    company: string;
+    position: string;
+    startDate: string;
+    endDate: string;
+    description?: string[];
+  };
+  export let exp: Experience;
+</script>
+
+<div class="my-4">
+  <div class="flex flex-row w-full">
+    <span class="grow block">
+      <b>{exp.position}</b> - {exp.company}
+    </span>
+    <span class="grow text-right">{exp.startDate} - {exp.endDate}</span>
+  </div>
+  {#if exp.description}
+    <ul class="pl-4 list-disc list-inside">
+      {#each exp.description as desc}
+        <li>{desc}</li>
+      {/each}
+    </ul>
+  {/if}
+</div>
