dbf313177a9e9a16f3bf8510b7f39dc3ad249385
Frank Li <frankla0203@outlook.com>
2024-01-06 15:28:27 -0800

    Move owl

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -6,10 +6,13 @@
 </script>
 
 <main
-  class="flex flex-row lg:px-8 lg:mx-auto w-full lg:max-w-5xl lg:gap-20 overflow-hidden text-sm md:text-base lg:text-lg"
+  class="flex flex-row lg:px-8 lg:mx-auto w-full lg:max-w-5xl lg:gap-20 overflow-hidden text-sm md:text-lg"
 >
   <Sidebar bind:overlayVisible />
   <Content bind:overlayVisible>
     <slot />
   </Content>
+  <div class="hidden md:block md:absolute md:bottom-8 md:right-8">
+    <img src="/owl.png" alt="Owl" width="250" height="250" class="mx-auto" />
+  </div>
 </main>
