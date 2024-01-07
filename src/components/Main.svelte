f1c079b723f598a0938934393a150ee6f63eb27e
Frank Li <frankla0203@outlook.com>
2024-01-07 01:27:03 -0800

    Semiresponsive owl

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -3,16 +3,29 @@
   import Content from "./Content.svelte";
 
   let overlayVisible = false;
+  let mainContainer: HTMLElement;
+  let innerWidth = window.innerWidth;
+
+  $: owlSize = Math.min(
+    250,
+    mainContainer ? (innerWidth - mainContainer.clientWidth) / 2 : 0
+  );
 </script>
 
 <main
-  class="flex flex-row lg:px-8 lg:mx-auto w-full lg:max-w-5xl lg:gap-20 overflow-hidden text-sm md:text-lg"
+  class="flex flex-row lg:px-8 lg:mx-auto w-full lg:max-w-5xl lg:gap-14 overflow-hidden text-sm md:text-lg"
+  bind:this={mainContainer}
 >
   <Sidebar bind:overlayVisible />
   <Content bind:overlayVisible>
     <slot />
   </Content>
   <div class="hidden md:block md:absolute md:bottom-8 md:right-8">
-    <img src="/owl.png" alt="Owl" width="250" height="250" class="mx-auto" />
+    <img
+      src="/owl.png"
+      alt="Owl"
+      width={owlSize ? owlSize : 0}
+      height={owlSize ? owlSize : 0}
+    />
   </div>
 </main>
