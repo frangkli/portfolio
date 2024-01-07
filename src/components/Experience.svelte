78f65d6d972d6d9f0d735d23770210fcd1252e2b
Frank Li <frankla0203@outlook.com>
2024-01-06 23:04:12 -0800

    CV experiences section

--- src/components/Experience.svelte
+++ src/components/Experience.svelte
@@ -4,23 +4,23 @@
     position: string;
     startDate: string;
     endDate: string;
-    description?: string[];
   };
   export let exp: Experience;
 </script>
 
-<div class="my-4">
-  <div class="flex flex-row w-full">
+<div class="mb-4">
+  <div class="flex flex-row md:hidden">
+    <span class="grow block">
+      <b>{exp.position}</b>
+    </span>
+    <span class="grow text-right">{exp.startDate} - {exp.endDate}</span>
+  </div>
+  <span class="md:hidden">{exp.company}</span>
+  <div class="hidden md:flex md:flex-row w-full">
     <span class="grow block">
       <b>{exp.position}</b> - {exp.company}
     </span>
     <span class="grow text-right">{exp.startDate} - {exp.endDate}</span>
   </div>
-  {#if exp.description}
-    <ul class="pl-4 list-disc list-inside">
-      {#each exp.description as desc}
-        <li>{desc}</li>
-      {/each}
-    </ul>
-  {/if}
+  <slot />
 </div>
