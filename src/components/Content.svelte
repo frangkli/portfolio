ffd95e92ba3d5cb0bcf71f91f843e2331bf51b2a
Frank Li <frankla0203@outlook.com>
2024-01-06 20:50:38 -0800

    Fix broken intro animation

--- src/components/Content.svelte
+++ src/components/Content.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import { fly } from "svelte/transition";
 
   export let overlayVisible: boolean;
+  let mounted = false;
+  onMount(() => {
+    mounted = true;
+  });
 </script>
 
-{#if !overlayVisible}
+{#if !overlayVisible && mounted}
   <div
     class="h-max px-8 lg:px-0 py-20 lg:py-32 text-justify w-full"
     in:fly={{ y: 200, duration: 1000 }}
