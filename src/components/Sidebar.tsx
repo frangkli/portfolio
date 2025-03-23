fde5b15e28492f38e880a19328cb48955cfc3ed3
Frank Li <frankla0203@outlook.com>
2025-03-23 01:31:57 -0700

    make it look better on mobile

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -23,22 +23,22 @@ const links = [
 
 export function Sidebar() {
   return (
-    <aside className="min-w-fit text-right">
-      <div className="flex flex-col h-1/2 pt-16">
+    <aside className="min-w-fit text-center md:text-right">
+      <div className="flex flex-col h-1/2 pt-16 gap-4">
         <div className="grow">
           <h1 className="text-5xl font-cursive italic mb-6">Frank Li</h1>
-          <nav>
-            <ul className="space-y-2">
+          <nav className="not-md:flex not-md:flex-row not-md:flex-wrap not-md:justify-center">
+            <ul className="not-md:space-x-2 md:space-y-2">
               {links.map((link) => (
-                <li key={link.href}>
+                <li key={link.href} className="not-md:inline">
                   <HeadingLink href={link.href}>{link.label}</HeadingLink>
                 </li>
               ))}
             </ul>
           </nav>
         </div>
-        <div className="text-left text-sm text-neutral-700 dark:text-neutral-300">
-          <div className="flex flex-row gap-5">
+        <div className="md:text-left text-sm text-neutral-700 dark:text-neutral-300">
+          <div className="flex flex-row gap-5 not-md:justify-center">
             <HeadingLink href="https://github.com/frangkli">
               <GitHub size={ICON_SIZE} />
             </HeadingLink>
