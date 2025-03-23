e6671a3245fb3965dbc9e72854e89727ac58100b
Frank Li <frankla0203@outlook.com>
2025-03-23 11:41:30 -0700

    use only one h1 heading, changed others to h2

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,8 +2,8 @@ import type { PropsWithChildren } from "react";
 
 export function Heading(props: PropsWithChildren) {
   return (
-    <h1 className="text-sm font-bold text-neutral-400 py-4">
+    <h2 className="text-sm font-bold text-neutral-400 py-4">
       {props.children}
-    </h1>
+    </h2>
   );
 }
